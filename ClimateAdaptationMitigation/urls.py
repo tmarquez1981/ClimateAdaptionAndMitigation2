386ccaf0af5b4e6860c14b90400618119f258eac
Eugene Han <ehan2@alaska.edu>
2017-10-31 14:03:52 -0800

    Change html files and url.py

--- ClimateAdaptationMitigation/urls.py
+++ ClimateAdaptationMitigation/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url
 from django.views.generic import TemplateView
 
 urlpatterns = [
+
     url(r'^home', TemplateView.as_view(template_name="ClimateAdaptationMitigation/html/home.html")),
     url(r'^map', TemplateView.as_view(template_name="ClimateAdaptationMitigation/html/map.html")),
-
+    url(r'^contact', TemplateView.as_view(template_name="ClimateAdaptationMitigation/html/contact.html")),
+    url(r'^about', TemplateView.as_view(template_name="ClimateAdaptationMitigation/html/about.html")),
 
 ]
